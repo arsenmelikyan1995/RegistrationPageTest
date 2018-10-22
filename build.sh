358da0dd6a23b7133d7a8890d62fe4944f1372b0
Arsen <arsenmelikyan5@gmail.com>
2018-10-23 00:21:49 +0400

    Chang build file

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 origin_jar_name="RegistrationPage-0.0.1.war"
 rename_jar_name="RegistrationPage.war"
 
-echo "########## BUILDING JAR FILE ##########"
+echo "########## BUILDING WAR FILE ##########"
 mvn clean package install
 
-echo "########## RENAME JAR FILE ##########"
-mv target/$origin_jar_name  /var/lib/tomcat8/webapps/$rename_jar_name
+echo "########## STOP TOMCAT ##########"
+sudo service tomcat8 stop
 
+echo "########## MOVE WAR FILE ##########"
+sudo rm -rf /var/lib/tomcat8/webapps/$rename_war_name*
+sudo mv target/$origin_war_name  /var/lib/tomcat8/webapps/$rename_war_name
+
+echo "########## START TOMCAT ##########"
+sudo service tomcat8 start
